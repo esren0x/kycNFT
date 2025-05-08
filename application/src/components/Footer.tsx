c865f91e3d8c76268cbd7ebb8dd78a00edd0c2ba
“David <dms.120@hotmail.com>
2025-05-08 16:27:30 +0100

    year

--- application/src/components/Footer.tsx
+++ application/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => (
   <footer className="bg-gray-50 border-t py-6 mt-auto">
     <div className="container mx-auto px-4 text-center text-gray-600">
-      <p>© 22025 Aleo KYC. All rights reserved.</p>
+      <p>© 2025 Aleo KYC. All rights reserved.</p>
     </div>
   </footer>
 );
